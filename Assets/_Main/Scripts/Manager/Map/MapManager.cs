4b3480ac3ea447dd272c3c998499b0d0535b3f2e
huytq <tranquanghuy12110@gmail.com>
2023-11-02 17:26:51 +0700

    u

---
+++ Assets/_Main/Scripts/Manager/Map/MapManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleGame
+{
+    public class MapManager : MonoBehaviour
+    {
+        private Texture2D NormalMap(Texture2D source, float strength)
+        {
+            strength = Mathf.Clamp(strength, 0.0F, 1.0F);
+
+            Texture2D normalTexture;
+            float xLeft;
+            float xRight;
+            float yUp;
+            float yDown;
+            float yDelta;
+            float xDelta;
+
+            normalTexture = new Texture2D(source.width, source.height, TextureFormat.ARGB32, true);
+
+            for (int y = 0; y < normalTexture.height; y++)
+            {
+                for (int x = 0; x < normalTexture.width; x++)
+                {
+                    xLeft = source.GetPixel(x - 1, y).grayscale * strength;
+                    xRight = source.GetPixel(x + 1, y).grayscale * strength;
+                    yUp = source.GetPixel(x, y - 1).grayscale * strength;
+                    yDown = source.GetPixel(x, y + 1).grayscale * strength;
+                    xDelta = ((xLeft - xRight) + 1) * 0.5f;
+                    yDelta = ((yUp - yDown) + 1) * 0.5f;
+                    normalTexture.SetPixel(x, y, new Color(xDelta, yDelta, 1.0f, yDelta));
+                }
+            }
+            normalTexture.Apply();
+
+            //Code for exporting the image to assets folder
+            System.IO.File.WriteAllBytes("Assets/NormalMap.png", normalTexture.EncodeToPNG());
+
+            return normalTexture;
+        }
+    }
+}
